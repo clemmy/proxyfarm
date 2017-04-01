b24a48d26ade3733b1d93f2953472aaeae718102
Clement Hoang <clement.hoang24@gmail.com>
2017-04-01 18:42:24 +0900

    Take CLI arguments and add default sources

--- proxyfarm.js
+++ proxyfarm.js
@@ -1,5 +1,8 @@
-var fs = require('fs');
+#!/usr/bin/env node --harmony
+
+const fs = require('fs');
 const phantom = require('phantom');
+const program = require('commander');
 
 const GET_NICE_TEXT_JS = `function() {
   var s = window.getSelection();
@@ -10,32 +13,49 @@ const GET_NICE_TEXT_JS = `function() {
   return s.toString();
 }`;
 
-var re = /([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3})(\s+|:)([0-9]{2,5})/g
+var re = /([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3})(\s+|:)([0-9]{2,5})/g;
 
-// TODO: Read proxy list
-// TODO: Improve resilience to errors
+// Get user arguments
+program
+  .version('0.0.1')
+  .description('Farm and validate proxies from custom proxy list sources.')
+  .option('-i, --in <type>', 'filepath of proxy list sources')
+  .option('-o, --out <type>', 'path to output proxy list')
+  .parse(process.argv);
+
+const proxyListSourcesRaw = fs.readFileSync(program.in || 'defaults/sources.txt', {encoding: 'utf8'});
+const proxyListSources = proxyListSourcesRaw.split(/\r?\n/).filter(line => !!line);
+console.log(`Scraping from ${proxyListSources.length} sources...`);
 
 async function main() {
-    const instance = await phantom.create();
-    const page = await instance.createPage();
+  const instance = await phantom.create();
+  const page = await instance.createPage();
 
-    console.log('Requesting page...');
-    const status = await page.open('http://proxydb.net/?availability=90');
-    console.log(`Page status: status`);
+  for (const source of proxyListSources) {
+    try {
+      console.log(`Requesting page ${source}...`);
+      const status = await page.open(source);
+      console.log(`Page status: ${status}`);
+      if (status !== 'success')
+        throw new Error('Request failed');
 
-    const jsEnabled = await page.setting('javascriptEnabled');
-    console.log(`Javascript enabled: ${jsEnabled}`);
+      const jsEnabled = await page.setting('javascriptEnabled');
+      console.log(`Javascript enabled: ${jsEnabled}`);
+      if (!jsEnabled)
+        throw new Error('Javascript disabled');
 
-    const niceText = await page.evaluateJavaScript(GET_NICE_TEXT_JS);
-    console.log(niceText);
+      const niceText = await page.evaluateJavaScript(GET_NICE_TEXT_JS);
+      const matches = niceText.match(re) || [];
+      console.log(`Proxies found on page: ${matches.length}`);
 
-    const matches = niceText.match(re) || [];
-    console.log(`Proxies found on page: ${matches.length}`);
+      const proxies = matches.map(s => s.replace(/\s+/, ':'));
+    } catch(e) {
+      console.log(`An error occurred while scraping ${source}. Skipping.`);
+      continue;
+    }
+  }
 
-    const proxies = matches.map(s => s.replace(/\s+/, ':'));
-    console.log(proxies);
-
-    await instance.exit();
+  await instance.exit();
 }
 
 main();
